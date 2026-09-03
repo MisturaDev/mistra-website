36935ca79389d5ccd68090f2473b5751639813c5
TurahRae <Turahrae@gmail.com>
2026-09-03 10:03:59 +0100

    refactor: streamline page structure and remove obsolete sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import ProblemSolution from "@/components/ProblemSolution";
-import BentoGrid from "@/components/BentoGrid";
-import FeaturePillars from "@/components/FeaturePillars";
 import DownloadSection from "@/components/DownloadSection";
-import Pricing from "@/components/Pricing";
 import CTASection from "@/components/CTASection";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
@@ -16,11 +12,7 @@ export default function Home() {
     <main className="min-h-screen bg-white text-slate-900 relative">
       <Navbar />
       <Hero />
-      <ProblemSolution />
-      <BentoGrid />
-      <FeaturePillars />
       <DownloadSection />
-      <Pricing />
       <CTASection />
       <FAQ />
       <Footer />
